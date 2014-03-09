8d68014ca8f1250dbce960a768cbed2a454a5782
cfiorio <cfiorio@mac.com>
2014-03-09 14:24:53 +0100

    Initial Commit

---
+++ Polytech SalonTests/Polytech_SalonTests.m
@@ -0,0 +1,34 @@
+//
+//  Polytech_SalonTests.m
+//  Polytech SalonTests
+//
+//  Created by Fiorio Christophe on 09/03/2014.
+//  Copyright (c) 2014 Fiorio Christophe. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Polytech_SalonTests : XCTestCase
+
+@end
+
+@implementation Polytech_SalonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
